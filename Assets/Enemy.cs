d32b82aed91227a960ab94c013af36f8e6d67301
trong.tn178@gmail.com <trong.tn178@gmail.com>
2019-12-28 10:19:51 +0700

    init

---
+++ Assets/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float health = 4f;
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        Debug.Log(collisionInfo.relativeVelocity.magnitude);
+        if (collisionInfo.relativeVelocity.magnitude > health)
+        {
+            Debug.Log(collisionInfo.relativeVelocity.magnitude);
+            Die();
+        }
+        
+        // Debug.Log(collisionInfo.relativeVelocity.magnitude);
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
